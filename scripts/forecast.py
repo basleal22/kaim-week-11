31f4542259fc17bef36435282568e54f7147f3f2
basleal22 <solibaslu@gmail.com>
2025-02-28 12:54:01 +0300

    wrote arimaand s arima models for our data

--- scripts/forecast.py
+++ scripts/forecast.py
@@ -6,6 +6,7 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from sklearn.metrics import mean_absolute_error,mean_squared_error, mean_absolute_percentage_error
 from math import sqrt
 import matplotlib.pyplot as plt
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 #we split our data into test and train
 def split_test_train(data):
     split_ratio=0.8
@@ -14,8 +15,11 @@ def split_test_train(data):
     train = data.iloc[:split_index]
     test = data.iloc[split_index:]
     return train,test
-#def arima_model(train,test):
+def arima_model(train,test):
     arima_m = ARIMA(train,order=(1,1,1))
-    arima_fit = arima_model.fit()
+    arima_fit = arima_m.fit()
     return arima_fit
-#def sarima_model(train,test)
+def sarima_model(train,test):
+    sarima_m= SARIMAX(train,seasonal_order=(1,1,1,12))
+    sarima_fit=sarima_m.fit()
+    return sarima_fit
